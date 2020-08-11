1b13462f6c13ca3a4925d92f1125d7c528c07e9e
Kosiarz101 <pol1100@o2.pl>
2020-08-11 13:00:38 +0200

    Default Start

--- src/Sorting/Main.java
+++ src/Sorting/Main.java
@@ -9,7 +9,19 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-        
+        int arr[] = {5,3,4,2,1,6};
+        //fillTheArray(arr);
+	    printarray(arr);
+	    long tStart = System.nanoTime();
+        Merge mr = new Merge();
+        mr.sort(arr, 0, arr.length - 1);
+        //Insertion.sort(arr);
+        //Quick.sort(arr, 0, arr.length-1);
+        long tEnd = System.nanoTime();
+        long elapsedtime = tEnd - tStart;
+        System.out.println("\n");
+        printarray(arr);
+        System.out.println("\nTime Elapsed: " + elapsedtime);
     }
     public static void printarray(int arr[]){
         for (byte i=0; i<arr.length; i++) {
