a8adf94519de9471d26143e822708b240756e56e
Kosiarz101 <pol1100@o2.pl>
2020-08-11 12:48:41 +0200

    Added QuickSort to project

--- src/Sorting/Main.java
+++ src/Sorting/Main.java
@@ -2,23 +2,25 @@ package Sorting;
 
 import Sorting.Insertion.Insertion;
 import Sorting.Merge.Merge;
+import Sorting.Quick.Quick;
 
 import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
-        int arr[] = new int[10000];
-        fillTheArray(arr);
-	    //printarray(arr);
+        int arr[] = {5,3,4,2,1,6};
+        //fillTheArray(arr);
+	    printarray(arr);
 	    long tStart = System.nanoTime();
-        Merge mr = new Merge();
-	    mr.sort(arr, 0, arr.length - 1);
+        //Merge mr = new Merge();
+	    // mr.sort(arr, 0, arr.length - 1);
         //Insertion.sort(arr);
+        Quick.sort(arr, 0, arr.length-1);
         long tEnd = System.nanoTime();
         long elapsedtime = tEnd - tStart;
         System.out.println("\n");
-        //printarray(arr);
+        printarray(arr);
         System.out.println("\nTime Elapsed: " + elapsedtime);
     }
     public static void printarray(int arr[]){
@@ -30,7 +32,7 @@ public class Main {
         Random number = new Random();
         for(int i=0; i<arr.length; i++)
         {
-            arr[i] = number.nextInt(1000);
+            arr[i] = number.nextInt(100);
         }
     }
 }
